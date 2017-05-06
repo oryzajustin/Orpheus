26ee2f1d90a417e6bbb0bca765c6641a88e041db
andusy <andus401@gmail.com>
2017-05-06 14:49:58 -0400

    Added scripts for the music bar and beat detection

---
+++ Orpheus/Assets/Scripts/Beat.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Runtime.InteropServices;
+
+public class Beat : MonoBehaviour
+{
+
+    //Object Physics
+    private Rigidbody2D rbdy;
+    public Collider2D beat_Collider;
+
+    //Speeds of the beats
+    private float speed;
+    private float[] bpm = new float[] { 130f, 120f, 141f, 166.76f, 90.15f, 140.01f, 129.95f, 149.81f };
+    /* Songs in order
+        0.  Destractor
+        1.  BossMain
+        2.  ChipTune
+        3.  Mercury
+        4.  Jump to Win
+        5.  Pocket Destroyer
+        6.  Unending Strike
+        7.  Venus
+     */
+    public int song;
+
+    //Location and orientation of the new beats to be spawned
+    private Vector2 loc;
+    private Quaternion rot;
+
+    // Use this for initialization
+    void Start()
+    {
+        //Colliders
+        rbdy = GetComponent<Rigidbody2D>();
+        beat_Collider = GetComponent<Collider2D>();
+
+        //Spead of beats
+        song = 7;
+        speed = bpm[song] / 10;
+
+        //Location for new beats to be spawned
+        loc = new Vector2(10.5f, -3.35f);
+        rot = new Quaternion(0, 0, 0, 0);
+
+        //Calls Instantiate function for 0.5s and repeats every 0s
+        InvokeRepeating("Instantiate", 60 / bpm[song], 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Setting speed of the beats
+        float hor = 0 - speed;
+        float vert = 0;
+        Vector2 beat = new Vector2(hor, vert);
+        rbdy.velocity = beat;
+    }
+
+    //Deletes the beats once it hits the end of the music bar
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name == "Music_Bar")
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    //Creates new bubbles at the specified location
+    private void Instantiate()
+    {
+        Instantiate(gameObject, loc, rot);
+    }
+}
---
+++ Orpheus/Assets/Scripts/Music_Bar.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Music_Bar : MonoBehaviour
+{
+
+    public Collider2D bar_Collider;
+    public CircleCollider2D cir_Collider;
+
+    // Use this for initialization
+    void Start()
+    {
+        bar_Collider = GetComponent<Collider2D>();
+        cir_Collider = GetComponent<CircleCollider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    //Detect if the space key is pressed at the correct time
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (Input.GetKeyDown("space"))
+        { //Detect space key
+            //Debug.Log("boi boi boi");
+            //Enter code for combo here
+        }
+    }
+}
