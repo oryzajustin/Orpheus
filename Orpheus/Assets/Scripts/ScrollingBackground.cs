7406d25e749022745fe72a04b11a8176cb613492
andusy <andus401@gmail.com>
2017-05-06 11:28:20 -0400

    Added scrolling background script

---
+++ Orpheus/Assets/Scripts/ScrollingBackground.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollingBackground : MonoBehaviour
+{
+
+    public bool scrolling, parallax;
+
+    public float size;//Background size
+    public float parallaxSpd;
+
+    private Transform cameraTransform;
+    private Transform[] layers;
+    private float view = 10;
+    private int left;
+    private int right;
+    private float lastCameraX;
+
+    public void Start()
+    {
+        cameraTransform = Camera.main.transform;
+        lastCameraX = cameraTransform.position.x;
+        layers = new Transform[transform.childCount];
+
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            layers[i] = transform.GetChild(i);
+        }
+
+        left = 0;
+        right = layers.Length - 1;
+    }
+
+    public void Update()
+    {
+        if (parallax)
+        {
+            float deltaX = cameraTransform.position.x - lastCameraX;
+            transform.position += Vector3.right * (deltaX * parallaxSpd);
+        }
+
+        lastCameraX = cameraTransform.position.x;
+
+        if (scrolling)
+        {
+            if (cameraTransform.position.x < (layers[left].transform.position.x + view))
+            {
+                scrollLeft();
+            }
+            if (cameraTransform.position.x > (layers[right].transform.position.x - view))
+            {
+                scrollRight();
+            }
+        }
+    }
+
+    private void scrollLeft()
+    {
+        int lastRight = right;
+        layers[right].position = Vector3.right * (layers[left].position.x - size);
+        left = right;
+        right--;
+        if (right < 0)
+        {
+            right = layers.Length - 1;
+        }
+    }
+
+    private void scrollRight()
+    {
+        int lastLeft = left;
+        layers[left].position = Vector3.right * (layers[right].position.x + size);
+        right = left;
+        left++;
+        if (left == layers.Length)
+        {
+            left = 0;
+        }
+    }
+
+
+}
